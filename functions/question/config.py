b37b91020a82e281d2cf9607b790100d4c1a74a7
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-27 12:22:37 -0300

    first commit

--- functions/question/config.py
+++ functions/question/config.py
@@ -7,7 +7,7 @@ class QuestionConfig:
             name="Question",
             path="./functions/question",
             description="Make rag questions about lforge",
-            layers=[services.layers.chromadb1, services.layers.chromadb2],
+            layers=[services.layers.chromadb1],
             
         )
 
